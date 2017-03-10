723791f6c4e2c98daa78b7e2bee89e948818a688
Louis Kirsch <louis@louiskirsch.com>
2017-03-10 18:17:42 +0100

    Reset vehicle to position a few seconds ago instead of the start

--- rl-race-virtual-env/Assets/CarState.cs
+++ rl-race-virtual-env/Assets/CarState.cs
@@ -4,19 +4,40 @@ using UnityEngine;
 
 public class CarState : MonoBehaviour {
 
-	private Vector3 initialPosition;
-	private Quaternion initialRotation;
+	private class TransformState {
+		public Vector3 position;
+		public Quaternion rotation;
+		
+		public TransformState(Vector3 position, Quaternion rotation) {
+			this.position = position;
+			this.rotation = rotation;
+		}
+	}
 
 	public bool Disqualified { get; private set; }
 	public bool Finished { get; private set; }
 
+	public uint transformHistoryLength = 7;
+	public float transformHistorySaveInterval = 1f;
+
 	private GameObject[] checkpoints;
 	private int checkpointsPassed = 0;
+	private TransformState[] transformHistory;
+	private int historyIndex = 0;
 
 	void Start () {
-		initialPosition = transform.position;
-		initialRotation = transform.rotation;
 		checkpoints = GameObject.FindGameObjectsWithTag("Checkpoint");
+
+		TransformState startState = new TransformState(transform.position, transform.rotation);
+		InitializeTransformHistory(startState);
+		InvokeRepeating("SaveTransform", 0f, transformHistorySaveInterval);
+	}
+
+	private void InitializeTransformHistory(TransformState state) {
+		transformHistory = new TransformState[transformHistoryLength];
+		for(int i = 0; i < transformHistoryLength; i++) {
+			transformHistory[i] = state;
+		}
 	}
 
 	public void ResetState() {
@@ -24,12 +45,28 @@ public class CarState : MonoBehaviour {
 		Finished = false;
 	}
 
+	private void SaveTransform() {
+		if(Disqualified)
+			return;
+		TransformState state = new TransformState(transform.position, transform.rotation);
+		transformHistory[historyIndex] = state;
+		historyIndex = (historyIndex + 1) % transformHistory.Length;
+	}
+
 	private void ResetVehicle() {
-		transform.position = initialPosition;
-		transform.rotation = initialRotation;
+		CarController carController = GetComponent<CarController>();
 		Rigidbody body = GetComponent<Rigidbody>();
+		TransformState historicalState = transformHistory[historyIndex];
+
+		// Reset history, throw away bad states that are close to disqualification
+		InitializeTransformHistory(historicalState);
+
+		carController.ApplyAction(0f, 0f);
+		transform.position = historicalState.position;
+		transform.rotation = historicalState.rotation;
 		body.velocity = Vector3.zero;
 		body.angularVelocity = Vector3.zero;
+		carController.ApplyAction(1f, 0f);
 	}
 
 	private void ResetCheckpoints() {
