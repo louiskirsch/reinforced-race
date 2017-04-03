7a6dd0d43fbde685d777925a144b52066cdc6503
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-04-03 21:11:32 +0200

    Add MotorCommand and SensorReadings classes (Communication between bricks and pi)

---
+++ rl-race-lejos-exchange-objects/src/MotorCommand.java
@@ -0,0 +1,10 @@
+
+public class MotorCommand implements java.io.Serializable {
+
+	private static final long serialVersionUID = 6927688243828269412L;
+	
+	int motorFront;
+	int motorBack;
+	int motorLeft;
+	int motorRight;
+}
