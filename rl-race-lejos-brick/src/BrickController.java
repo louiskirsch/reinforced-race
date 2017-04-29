8671188c4f2c9f3c0c406c643fe5ebb35da7186e
Sebastian Bischoff <sebastian.bischoff@student.hpi.de>
2017-04-29 12:01:21 +0200

    Remove all stuff related to serialization

--- rl-race-lejos-brick/src/BrickController.java
+++ rl-race-lejos-brick/src/BrickController.java
@@ -1,9 +1,26 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+import lejos.nxt.comm.NXTConnection;
+import lejos.nxt.comm.USB;
+import lejos.nxt.comm.USBConnection;
+
 
 public class BrickController {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		NXTConnection connection = USB.waitForConnection();
+		
+		DataOutputStream dataOut = connection.openDataOutputStream();
+		DataInputStream dataIn = connection.openDataInputStream();
+		
+		SensorReadings reading = new SensorReadings();
+		
+		reading.red = 5;
+		reading.green = 5;
+		reading.blue = 5;
+		
+		//object.write
 	}
 
 }
