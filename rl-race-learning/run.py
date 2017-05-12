ea236895b952e698aab712781910f7379b8b553f
Louis Kirsch <louis@louiskirsch.com>
2017-05-12 18:06:44 +0200

    Adapt defaut parameters to emotional memory and fast falling epsilon
    Currently the best working parameters

--- rl-race-learning/run.py
+++ rl-race-learning/run.py
@@ -19,25 +19,19 @@ parser.add_argument('--memory-capacity', dest='memory_capacity', type=int, defau
                     help='The size of the memory to hold past experiences')
 parser.add_argument('--image-size', dest='image_size', type=int, default=64,
                     help='The size of the (square) images to request from the environment')
-parser.add_argument('--rap-initial', dest='random_action_prob_initial', type=float, default=1.0,
-                    help='The initial probability of choosing a random action instead')
-parser.add_argument('--rap-target', dest='random_action_prob_target', type=float, default=0.1,
-                    help='The probability of choosing a random action after annealing period has passed')
-# Atari annealing period was 1M
-# It seems like there is not much progress being made in that manner, 10k should be enough
-parser.add_argument('--rap-annealing-period', dest='random_action_prob_annealing_period',
-                    type=int, default=10000,
-                    help='The length of the random action annealing period in frames')
-parser.add_argument('--rap-annealing', dest='use_rap_annealing', action='store_true',
-                    help='Use an annealing random action policy instead of `terminal distance`')
-parser.add_argument('--rap-terminal-count', dest='rap_terminal_episode_count',
-                    type=int, default=20,
-                    help='Use the given moving average episode count for the policy')
+# Atari annealing period was 1M, initial 1.0 and target 0.1
+parser.add_argument('--rap-annealing', dest='rap_annealing', metavar='INITIAL TARGET ANNEALING_PERIOD',
+                    type=float, nargs=3, default=[1.0, 0.0, 1000],
+                    help='Use an annealing random action policy with INITIAL probability, and TARGET probability '
+                         'after annealed over ANNEALING_PERIOD frames')
+parser.add_argument('--rap-terminal-count', dest='rap_terminal_episode_count', type=int,
+                    help='Use the given moving average episode count for the random policy instead. '
+                         '20 is a good default.')
 parser.add_argument('--rap-reuse', dest='rap_reuse_prob', type=float, default=0.8,
                     help='Enable reusing random actions with the given probability')
-parser.add_argument('--emotional-memory', dest='emotional_memory_length', type=int, default=0,
-                    help='Enable emotional memory that highlights previous failures'
-                         'with the given length in frames per event')
+parser.add_argument('--emotional-memory', dest='emotional_memory_length', type=int, default=20,
+                    help='Emotional memory that highlights previous failures '
+                         'with the given length in frames per event. Set to 0 to disable.')
 parser.add_argument('--batch-size', dest='batch_size',
                     type=int, default=32,
                     help='The minibatch size to use for training')
@@ -69,12 +63,11 @@ args = parser.parse_args()
 
 environment = EnvironmentInterface(args.host, args.port)
 
-if not args.use_rap_annealing:
+if args.rap_terminal_episode_count:
     random_action_policy = TerminalDistanceRAPolicy(args.rap_terminal_episode_count)
 else:
-    random_action_policy = AnnealingRAPolicy(args.random_action_prob_initial,
-                                             args.random_action_prob_target,
-                                             args.random_action_prob_annealing_period)
+    initial, target, period = args.rap_annealing
+    random_action_policy = AnnealingRAPolicy(initial, target, period)
 
 if args.rap_reuse_prob:
     random_action_policy = ReuseRAPolicyDecorator(random_action_policy,
